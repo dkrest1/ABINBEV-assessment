aebb1e1c61f61a8c3a2ee6c8cd31eaff51ece6d6
Akande Oluwatosin <aktosin1@gmail.com>
2023-02-11 21:23:26 +0100

    dockerize the customer services

--- customer/src/config/dbConfig.js
+++ customer/src/config/dbConfig.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const sequelize = new Sequelize(
-    process.env.DATABASE,
-    process.env.USER,
-    process.env.PASSWORD, {
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: "localhost",
+    host: process.env.DB_HOST,
     pool: {
         max: 5,
         min: 0,
